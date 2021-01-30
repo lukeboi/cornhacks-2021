5d610f655c266b81942e809ff73aee4cad4cb4f7
Taylor <taylorbernt@gmail.com>
2021-01-30 16:40:03 -0600

    Working on fixing database schema, started working on left profile layout

--- cornhacks2021/web/src/Routes.js
+++ cornhacks2021/web/src/Routes.js
@@ -12,6 +12,18 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/projects/new" page={NewProjectPage} name="newProject" />
+      <Route path="/projects/{id:Int}/edit" page={EditProjectPage} name="editProject" />
+      <Route path="/projects/{id:Int}" page={ProjectPage} name="project" />
+      <Route path="/projects" page={ProjectsPage} name="projects" />
+      <Route path="/posts/new" page={NewPostPage} name="newPost" />
+      <Route path="/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
+      <Route path="/posts/{id:Int}" page={PostPage} name="post" />
+      <Route path="/posts" page={PostsPage} name="posts" />
+      <Route path="/users/new" page={NewUserPage} name="newUser" />
+      <Route path="/users/{id:Int}/edit" page={EditUserPage} name="editUser" />
+      <Route path="/users/{id:Int}" page={UserPage} name="user" />
+      <Route path="/users" page={UsersPage} name="users" />
       <Route path="/profile" page={ProfilePage} name="profile" />
       <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
