a0f1d2f2a185b21cd920e2c49f34f79f425231e4
jimmy.erickson <jimmy.erickson@huskers.unl.edu>
2021-01-30 15:24:40 -0600

    Second attempt at AddUpdate
    Uses tailwind this time

--- cornhacks2021/web/src/pages/HomePage/HomePage.js
+++ cornhacks2021/web/src/pages/HomePage/HomePage.js
@@ -1,22 +1,10 @@
 import { Link, routes } from '@redwoodjs/router'
-import { Navabr } from 'src/components/Navbar'
+//import { Navabr } from 'src/components/Navbar'
 import UserViewLayout from 'src/layouts/UserViewLayout/UserViewLayout'
+import AddContentLayout from 'src/layouts/AddContentLayout/AddContentLayout';
 
 const HomePage = () => {
-  return (
-    <>
-      <UserViewLayout></UserViewLayout>
-      <h1>Rokkit Home</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
-      {/* <Navbar></Navbar> */}
-    </>
-  )
+  return <AddContentLayout></AddContentLayout>
 }
 
 export default HomePage
