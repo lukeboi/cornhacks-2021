4462d8b2b4c726324af7aab88337ce88bbc6f567
jimmy.erickson <jimmy.erickson@huskers.unl.edu>
2021-01-30 14:41:51 -0600

    Initial attempt at AddPost
    Code from https://code.tutsplus.com/tutorials/creating-a-blogging-app-using-react-part-3-add-display-post--cms-28685

--- cornhacks2021/web/src/components/AddUpdate/AddUpdate.js
+++ cornhacks2021/web/src/components/AddUpdate/AddUpdate.js
@@ -1,8 +1,20 @@
 const AddUpdate = () => {
   return (
-    <div>
-      <h2>{'AddUpdate'}</h2>
-      <p>{'Find me in ./web/src/components/AddUpdate/AddUpdate.js'}</p>
+    <div className="col-md-5">
+      <div className="form-area">  
+        <form role="form">
+        <br styles="clear:both" />
+          <div className="form-group">
+            <input type="text" className="form-control" id="title" name="title" placeholder="Title" required />
+          </div>
+               
+          <div className="form-group">
+          <textarea className="form-control" type="textarea" id="subject" placeholder="Subject" maxlength="140" rows="7"></textarea>
+          </div>
+                  
+        <button type="button" id="submit" name="submit" className="btn btn-primary pull-right">Add Post</button>
+        </form>
+      </div>
     </div>
   )
 }
