2716270dd171a09c412d041e7786ee6752262b6d
jimmy.erickson <jimmy.erickson@huskers.unl.edu>
2021-01-31 02:18:39 -0600

    updated UserInfoHomePageCell to display profile info

--- web/src/components/UserInfoHomePageCell/UserInfoHomePageCell.js
+++ web/src/components/UserInfoHomePageCell/UserInfoHomePageCell.js
@@ -1,6 +1,7 @@
 export const QUERY = gql`
   query ($id: Int!) {
     user(id: $id) {
+      id
       name
       email
     }
@@ -50,24 +51,28 @@ export const Empty = () => {
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
 export const Success = ({ user }) => {
-  return (user) =>
-    <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-      <div class="px-4 py-5 sm:px-6">
-        <h3 class="text-lg leading-6 font-medium text-gray-900">
-          {user.name}
-        </h3>
+  console.log({user});
+  return (user =>
+    <article key={user.id}>
+      <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+        <div class="px-4 py-5 sm:px-6">
+          <h3 class="text-lg leading-6 font-medium text-gray-900">
+            {user.name}
+          </h3>
+        </div>
+        <div class="border-t border-gray-200">
+          <dl>
+            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">
+                Email
+              </dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {user.email}
+              </dd>
+            </div>
+          </dl>
+        </div>
       </div>
-      <div class="border-t border-gray-200">
-        <dl>
-          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt class="text-sm font-medium text-gray-500">
-              Email
-            </dt>
-            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {user.email}
-            </dd>
-          </div>
-        </dl>
-      </div>
-    </div>
+    </article>
+  );
 }
