c580989cbcefcef760f4c00a4e76395bbc2d5df1
Noah Costello <noahcostello5@gmail.com>
2021-01-31 02:46:51 -0600

    Color scheme corrections.

--- web/src/components/AddPost/AddPost.js
+++ web/src/components/AddPost/AddPost.js
@@ -35,7 +35,7 @@ const AddPost = () => {
           </div>
         </div>
         <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-          <button type="submit" onClick="addPost(document.getElementById('updateText'))" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button type="submit" onClick="addPost(document.getElementById('updateText'))" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Post Update
           </button>
         </div>
