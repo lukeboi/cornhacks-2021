a1af7f0df04c822d0d25c8eca2accc2c265e3372
Noah Costello <noahcostello5@gmail.com>
2021-01-31 02:20:32 -0600

    Generated navbar component.

---
+++ web/src/components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+const Navbar = () => {
+  return (
+    <header>
+      <nav class="bg-yellow-200 flex py-4 px-4">
+        <div class="pr-5">
+          <p class="text-3xl font-bold no-underline text-grey-darkest  hover:text-blue-dark">🚀 Rokkit</p>
+        </div>
+        <ul class="flex flex-row text-xl ">
+
+          <li class="pr-5">
+            <button
+              class="
+                font-semibold
+                px-3 py-1
+                border-b-4
+                border-yellow-200
+                transition duration-300 ease-in-out
+                hover:border-yellow-50">Feed</button>
+          </li>
+          <li class="pr-5">
+            <button
+              class="
+                font-semibold
+                px-3 py-1
+                border-b-4
+                border-yellow-200
+                transition duration-300 ease-in-out
+                hover:border-yellow-50">Projects</button>
+          </li>
+        </ul>
+
+        <button onClick={logOut} class="px-3 py-1 bg-yellow-100 absolute right-16 ">Logout</button>
+
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
