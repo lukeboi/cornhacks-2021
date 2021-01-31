6a8df021b9d0db52785c9adce7549006286f4cb6
Taylor <taylorbernt@gmail.com>
2021-01-31 01:52:45 -0600

    Posts are now showing

--- web/src/components/AddPost/AddPost.js
+++ web/src/components/AddPost/AddPost.js
@@ -1,41 +1,41 @@
 const AddPost = () => {
   return (
     <form action="#" method="POST">
-      <div class="shadow sm:rounded-md sm:overflow-hidden">
-        <div class="px-4 py-2 bg-white space-y-2 sm:p-6">
+      <div className="shadow sm:rounded-md sm:overflow-hidden">
+        <div className="px-4 py-2 bg-white space-y-2 sm:p-6">
           <div>
-            <label for="title" class="block text-sm font-medium text-gray-700">
+            <label htmlFor="title" className="block text-sm font-medium text-gray-700">
               Write about what you've been up to!
             </label>
-            <div class="mt-1">
-              <textarea id="updateText" name="updateText" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="  Update Text"></textarea>
+            <div className="mt-1">
+              <textarea id="updateText" name="updateText" rows="3" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="  Update Text"></textarea>
             </div>
           </div>
           <div>
-            <label class="block text-sm font-medium text-gray-700">
+            <label className="block text-sm font-medium text-gray-700">
               Add some media!
             </label>
-            <div class="mt-2 flex justify-center px-2 pt-2 pb-3 border-2 border-gray-300 border-dashed rounded-md">
-              <div class="space-y-1 text-center">
-                <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                  <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <div className="mt-2 flex justify-center px-2 pt-2 pb-3 border-2 border-gray-300 border-dashed rounded-md">
+              <div className="space-y-1 text-center">
+                <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                  <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
-                <div class="flex text-sm text-gray-600">
-                  <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                <div className="flex text-sm text-gray-600">
+                  <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                     <span>Upload a file</span>
-                    <input id="file-upload" name="file-upload" type="file" class="sr-only" multiple></input>
+                    <input id="file-upload" name="file-upload" type="file" className="sr-only" multiple></input>
                   </label>
-                  <p class="pl-1">or drag and drop</p>
+                  <p className="pl-1">or drag and drop</p>
                 </div>
-                <p class="text-xs text-gray-500">
+                <p className="text-xs text-gray-500">
                   Plz don't upload virus up to 10MB
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-          <button type="submit" onclick="addPost(document.getElementById('updateText').value)" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+          <button type="submit" onClick="addPost(document.getElementById('updateText'))" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Post Update
           </button>
         </div>
--- web/src/components/TimelinePostsCell/TimelinePostsCell.js
+++ web/src/components/TimelinePostsCell/TimelinePostsCell.js
@@ -1,24 +1,24 @@
+import { Link, routes } from '@redwoodjs/router'
+
+import Posts from 'src/components/Posts'
+
 export const QUERY = gql`
-    query {
-      posts {
-        id
-        bodyText
-        createdAt
-          Project {
-            name
-          }
-        projectId
-      }
+  query POSTS {
+    posts {
+      id
+      bodyText
+      createdAt
     }
+  }
 `
 
 export const Loading = () => {
   return (
-      <div class="mt-3 bg-white shadow overflow-hidden sm:rounded-lg">
-        <div class="border-t border-gray-200">
+      <div className="mt-3 bg-white shadow overflow-hidden sm:rounded-lg">
+        <div className="border-t border-gray-200">
           <dl>
-            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                 Loading...
               </dd>
             </div>
@@ -30,19 +30,19 @@ export const Loading = () => {
 
 export const Empty = () => {
   return (
-      <div class="mt-3 bg-white shadow overflow-hidden sm:rounded-lg">
-        <div class="px-4 py-5 sm:px-6">
-          <h3 class="text-lg leading-6 font-medium text-gray-900">
+      <div className="mt-3 bg-white shadow overflow-hidden sm:rounded-lg">
+        <div className="px-4 py-5 sm:px-6">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">
             No updates yet...
           </h3>
-          <p class="mt-1 max-w-2xl text-sm text-gray-500">
+          <p className="mt-1 max-w-2xl text-sm text-gray-500">
             But you could change that...
           </p>
         </div>
-        <div class="border-t border-gray-200">
+        <div className="border-t border-gray-200">
           <dl>
-            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                 Make a new post and let people know what you've been up to!
               </dd>
             </div>
@@ -54,22 +54,23 @@ export const Empty = () => {
 
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
-export const Success = ({ timelinePosts }) => {
-  return posts.map((post) => (
+export const Success = ({ posts }) => {
+  console.log({posts});
+  return posts.map(post =>
     <article key={post.id}>
-      <div class="mt-3 bg-white shadow overflow-hidden sm:rounded-lg">
-        <div class="px-4 py-5 sm:px-6">
-          <h3 class="text-lg leading-6 font-medium text-gray-900">
-            {post.Project.name}
+      <div className="mt-3 bg-white shadow overflow-hidden sm:rounded-lg">
+        <div className="px-4 py-5 sm:px-6">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">
+            {post.name}
           </h3>
-          <p class="mt-1 max-w-2xl text-sm text-gray-500">
+          <p className="mt-1 max-w-2xl text-sm text-gray-500">
             {post.createdAt}
           </p>
         </div>
-        <div class="border-t border-gray-200">
+        <div className="border-t border-gray-200">
           <dl>
-            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                 {post.bodyText}
               </dd>
             </div>
@@ -77,5 +78,5 @@ export const Success = ({ timelinePosts }) => {
         </div>
       </div>
     </article>
-  ))
+  );
 }
